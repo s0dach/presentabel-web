bb97337b8f054cd854c9786885b57961ad1bc937
Nikita Sidukov <nsidukov@icloud.com>
2023-02-05 02:04:08 +0300

    first changes

---
+++ src/constants/app-options.ts
@@ -0,0 +1,92 @@
+export const BASE_ITEMS = [
+  {
+    id: "templates",
+    name: "Templates",
+    label: "Шаблоны",
+  },
+  {
+    id: "customize",
+    name: "Customize",
+    label: "Кастом",
+  },
+  {
+    id: "elements",
+    name: "Elements",
+    label: "Элементы",
+  },
+  {
+    id: "images",
+    name: "Images",
+    label: "Картинки",
+  },
+  {
+    id: "uploads",
+    name: "Uploads",
+    label: "Загрузки",
+  },
+  {
+    id: "text",
+    name: "Text",
+    label: "Текст",
+  },
+  // {
+  //   id: "graphics",
+  //   name: "Graphics",
+  //   label: "Кастомизировать",
+  // },
+
+  // {
+  //   id: "pixabay",
+  //   name: "Pixabay",
+  // },
+  {
+    id: "layers",
+    name: "Layers",
+    label: "Слайды",
+  },
+]
+
+export const VIDEO_PANEL_ITEMS = [
+  {
+    id: "templates",
+    name: "Templates",
+  },
+  {
+    id: "customize",
+    name: "Customize",
+  },
+  {
+    id: "elements",
+    name: "Elements",
+  },
+  {
+    id: "images",
+    name: "Images",
+  },
+  {
+    id: "videos",
+    name: "Videos",
+  },
+  {
+    id: "uploads",
+    name: "Uploads",
+  },
+  {
+    id: "text",
+    name: "Text",
+  },
+  {
+    id: "graphics",
+    name: "Graphics",
+  },
+
+  {
+    id: "pixabay",
+    name: "Pixabay",
+  },
+]
+
+export enum PanelType {
+  TEMPLATES = "Templates",
+  BACKGROUND = "Background",
+}
